732a72261b8bede5a8db22735c8708474731ac6e
Arya Patil <arya.patil@medable.com>
2026-07-05 20:28:11 +0530

    done

--- server/server.js
+++ server/server.js
@@ -23,7 +23,22 @@ const app = express();
 const connectDB = require("./config/db");
 connectDB();  
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ethnique.netlify.app",
+    ],
+    methods: [
+      "GET",
+      "POST",
+      "PUT",
+      "DELETE",
+      "PATCH",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/admin", adminRoutes);
