81905268d29a64f7c41e84e635f4bd601b80a8c0
Arya Patil <arya.patil@medable.com>
2026-07-05 19:51:26 +0530

    Fix logout clearing cart and wishlist data

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const Product = require("../models/Product");
-const adminAuth = require("../middleware/adminAuth");
+const adminAuth = require("../middleware/Adminauth.js");
 const Admin = require("../models/Admin");
 
 const router = express.Router();
